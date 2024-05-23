a6d0d359f5cb15030c2f038d45806305ad38ac87
vishnu3de <vishnu21112001@gmail.com>
2024-05-23 16:52:27 +0530

    small change in manifest ag

--- survey_dgz/controllers/survey_controller.py
+++ survey_dgz/controllers/survey_controller.py
@@ -7,6 +7,8 @@ from odoo.addons.survey.controllers.main import Survey
 class SurveyExtended(Survey):
     @http.route('/survey/start/<string:survey_token>', type='http', auth='public', website=True)
     def survey_start(self, survey_token, answer_token=None, email=False, **post):
+        print('hi')
         survey_record = http.request.env['survey.survey'].search([('access_token', '=', survey_token)])
         survey_record.check_token_expiration()
+
         return super(SurveyExtended, self).survey_start(survey_token, answer_token, email, **post)
